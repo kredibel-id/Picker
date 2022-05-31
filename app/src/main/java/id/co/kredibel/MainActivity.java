ee49f553ae3da56dc77b9bebe553831880cd8d6e
Hangga Aji Sayekti <bazeniancode@gmail.com>
2022-06-01 06:14:55 +0700

    [Update] stable -> camera, gallery

--- app/src/main/java/id/co/kredibel/MainActivity.java
+++ app/src/main/java/id/co/kredibel/MainActivity.java
@@ -1,9 +1,17 @@
 package id.co.kredibel;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import io.kredibel.picker.Picker;
+import io.kredibel.picker.PickerListener;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView imgResult = findViewById(R.id.imgResult);
+        TextView txtPath = findViewById(R.id.txtPath);
 
-        findViewById(R.id.btnCamera).setOnClickListener(view -> picker.pickCamera((uri, file, bitmap)
-                -> imgResult.setImageURI(uri)
-        ));
+        findViewById(R.id.btnCamera).setOnClickListener(view -> picker.pickCamera(new PickerListener() {
+            @Override
+            public void onPicked(Uri uri, File file, Bitmap bitmap) {
+                imgResult.setImageURI(uri);
+                txtPath.setText(file.getAbsolutePath()+"(" + (file.exists()) + ")");
+                Log.d("CEK", "CEK-EXIST: Bitmap|Camera: " + (bitmap != null));
+                Log.d("CEK", "CEK-EXIST: File|Camera: " + (file.exists()) + "|" + file.getAbsolutePath());
+            }
+        }));
 
-        findViewById(R.id.btnGalery).setOnClickListener(view -> picker.pickGallery((uri, file, bitmap)
-                -> imgResult.setImageURI(uri))
-        );
+        findViewById(R.id.btnGalery).setOnClickListener(view -> picker.pickGallery(new PickerListener() {
+            @Override
+            public void onPicked(Uri uri, File file, Bitmap bitmap) {
+                imgResult.setImageURI(uri);
+                txtPath.setText(file.getAbsolutePath()+"(" + (file.exists()) + ")");
+                Log.d("CEK", "CEK-EXIST: Bitmap|Gallery: " + (bitmap != null));
+                Log.d("CEK", "CEK-EXIST: File|Gallery: " + (file.exists())+ "|" + file.getAbsolutePath());
+            }
+        }));
     }
 }
